97252eab63fa891142baef4866d8ee11344af0c2
Ulad Kasach <uladkasach@gmail.com>
2025-11-02 17:33:16 -0600

    fix(cicd): approve arch as a valid commmit prefix

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: { 'header-max-length': [1, 'always', 140] },
+  rules: {
+    'header-max-length': [1, 'always', 140],
+    'type-enum': [2, 'always', ['arch', 'chore', 'feat', 'fix', 'revert']],
+  },
 };
