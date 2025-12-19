0d4dba7d6d1e467ea73e75d6e4e2cd6021059277
Ulad Kasach <uladkasach@gmail.com>
2025-12-19 05:49:52 -0600

    fix(mechanic): sedreplace and boot repo=this/role=any on session start

--- rhachet.use.ts
+++ rhachet.use.ts
@@ -1,3 +1,6 @@
-import { getRoleRegistry } from './src/logic/roles/getRoleRegistry';
+import { InvokeHooks, RoleRegistry } from 'rhachet';
 
-export const getRoleRegistries = () => [getRoleRegistry()];
+import { getRoleRegistry as getRoleRegistryEhmpathy, getInvokeHooks as getInvokeHooksEhmpathy } from 'rhachet-roles-ehmpathy';
+
+export const getRoleRegistries = (): RoleRegistry[] => [getRoleRegistryEhmpathy()];
+export const getInvokeHooks = (): InvokeHooks[] => [getInvokeHooksEhmpathy()];
