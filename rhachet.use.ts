5e9626e767ed5ccf6dc03ce3644a3148a569d038
Ulad Kasach <uladkasach@gmail.com>
2025-12-26 08:12:36 -0600

    fix(mechanic): grat privs to acceptance test locally

--- rhachet.use.ts
+++ rhachet.use.ts
@@ -1,6 +1,8 @@
 import type { InvokeHooks, RoleRegistry } from 'rhachet';
 
 import { getRoleRegistry as getRoleRegistryEhmpathy, getInvokeHooks as getInvokeHooksEhmpathy } from './dist/index.js';
+import { getRoleRegistry as getRoleRegistryBhrain, getInvokeHooks as getInvokeHooksBhrain } from 'rhachet-roles-bhrain';
+import { getRoleRegistry as getRoleRegistryBhuild, getInvokeHooks as getInvokeHooksBhuild } from 'rhachet-roles-bhuild';
 
-export const getRoleRegistries = (): RoleRegistry[] => [getRoleRegistryEhmpathy()];
-export const getInvokeHooks = (): InvokeHooks[] => [getInvokeHooksEhmpathy()];
+export const getRoleRegistries = (): RoleRegistry[] => [getRoleRegistryEhmpathy(), getRoleRegistryBhrain(), getRoleRegistryBhuild()];
+export const getInvokeHooks = (): InvokeHooks[] => [getInvokeHooksEhmpathy(), getInvokeHooksBhrain(), getInvokeHooksBhuild()];
